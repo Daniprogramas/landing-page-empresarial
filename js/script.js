8a00b405d312acfa5826bae8b623178f18177585
Danielle Morais <111515555+Daniprogramas@users.noreply.github.com>
2025-12-19 12:04:50 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Exemplo simples de validação do formulário
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso!");
+});
